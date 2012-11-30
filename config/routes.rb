bfbadacc6f14dce8a0e1d6c143e05cf2c4f34a0e
Hartaarn Saini <hartaarn@gmail.com>
2012-11-29 22:34:25 -0500

    modified label, page, and button values

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Mywiki::Application.routes.draw do
 
   resources :pages
 
-  root :to => 'wiki#index'
+  root :to => 'pages#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
